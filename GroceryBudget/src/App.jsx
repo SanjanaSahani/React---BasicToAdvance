eb3082439f35c091964486fcef37f07dda2d0f14
Sanjana <sanjana6122002@gmail.com>
2025-03-28 12:56:48 +0530

    GroceryBudget

---
+++ GroceryBudget/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-4xl text-center font-bold'>Grocery Budget Project</h1>
+     
+    </>
+  )
+}
+
+export default App
