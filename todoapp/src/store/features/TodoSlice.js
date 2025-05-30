53df56a1b66b220dbc6af6e843774457404709ce
Sanjana <sanjana6122002@gmail.com>
2025-05-30 19:25:03 +0530

    todo

--- todoapp/src/store/features/TodoSlice.js
+++ todoapp/src/store/features/TodoSlice.js
@@ -11,9 +11,19 @@ const TodoSlice = createSlice({
         addTodo: (state, action) => {
             state.todos.push(action.payload)
         },
-        deleteTodo: (state, action) => { },
-        updateTodo: (state, action) => { },
-        clearTodo: (state, action) => { }
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        updateTodo: (state, action) => {
+            const { id, text } = action.payload;
+            const todo = state.todos.find((t) => t.id === id);
+            if (todo) {
+                todo.text = text;
+            }
+        },
+        clearTodo: (state, action) => {
+            state.todos = []
+        }
     }
 })
 export default TodoSlice.reducer;
