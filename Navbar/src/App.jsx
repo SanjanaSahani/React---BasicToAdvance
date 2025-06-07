0dbb0b7722b455715c82473f718a3f76a9a09ae3
Sanjana <sanjana6122002@gmail.com>
2025-06-07 20:10:48 +0530

    Navbar

--- Navbar/src/App.jsx
+++ Navbar/src/App.jsx
@@ -8,10 +8,10 @@ function App() {
 
   return (
     <>
-      <div className='lg:flex lg:gap-[10%] bg-white lg:w-full w-fit'>
-        <span className='flex text-4xl p-6 lg:text-3xl font-bold font-serif lg:pl-28 lg:pr-10 lg:py-2'>Coding <span className='text-blue-400'>Addict</span><span className='lg:hidden md:hidden ml-[42%]'><TiThMenu /></span></span>
+      <div className='lg:flex md:flex lg:gap-[10%] bg-white lg:w-full md:w-fit'>
+        <span className='flex text-4xl p-6 lg:text-3xl font-bold font-serif lg:pl-28 lg:pr-10 lg:py-2'>Coding <span className='text-blue-400'>Addict</span><span className='lg:hidden md:hidden'><TiThMenu /></span></span>
         
-        <span className='lg:flex lg:gap-9 font-serif lg:px-10 lg:py-4 text-md text-gray-400' >
+        <span className='lg:flex md:flex md:gap-8 md:px-10 md:py-4 lg:gap-9 font-serif lg:px-10 lg:py-4 text-md text-gray-400' >
           {links.map((link) => (
             <a className='gap-4 uppercase  hover:text-gray-500' key={link.id} href={link.url}>{link.text}</a>
           ))}
